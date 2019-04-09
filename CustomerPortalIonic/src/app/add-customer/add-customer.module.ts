915027a891337da7a581b11a15b5c617fc8b7e10
naveed <nksherani@outlook.com>
2019-04-10 02:14:39 +0500

    new page added

---
+++ CustomerPortalIonic/src/app/add-customer/add-customer.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { AddCustomerPage } from './add-customer.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AddCustomerPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [AddCustomerPage]
+})
+export class AddCustomerPageModule {}

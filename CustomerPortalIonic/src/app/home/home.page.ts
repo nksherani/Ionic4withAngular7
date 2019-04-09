fcd1688dc4c548da1f684b5014f0c2bd948028eb
naveed <nksherani@outlook.com>
2019-04-10 00:43:47 +0500

    index page

--- CustomerPortalIonic/src/app/home/home.page.ts
+++ CustomerPortalIonic/src/app/home/home.page.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    customers: Observable<any>;
+    custList = [];
+    constructor(public httpClient: HttpClient){
+      this.customers = this.httpClient.get('http://localhost:52146/api/customers');
+      this.customers.subscribe(data=>{
+        this.custList = data;
+        console.log(data);
+        
+      })
+    }
 
+    tempvar = "demo variable";
+
+    updateTemp=()=>{
+      this.tempvar = "changed demo var";
+    }
 }
